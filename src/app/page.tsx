8d6e52a1bb61384894b7132fc9a328498f5a20f3
Vaibhav Jain <jainvaibhav671@gmail.com>
2024-03-23 00:02:05 +0530

    Add Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,61 @@
+import PostItem from "@/components/PostItem";
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/lib/site";
+import { cn, sortPosts } from "@/lib/utils";
+import { posts } from "@site/content";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <section>Hello World</section>;
+  const latestPosts = sortPosts(posts).slice(0, 5);
+
+  return (
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+        <div className="container flex flex-col gap-4 text-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+            Hello, I&apos;m Vaibhav
+          </h1>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            Welcome to my blog page. Built using Tailwind, Shadcn, Velite and
+            NextJS-14
+          </p>
+          <div className="flex flex-col gap-4 justify-center sm:flex-row ">
+            <Link
+              href="/blog"
+              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
+            >
+              View my blogs
+            </Link>
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href={siteConfig.links.github}
+              className={cn(
+                buttonVariants({ size: "lg", variant: "outline" }),
+                "w-full sm:w-fit"
+              )}
+            >
+              Github
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2>
+        <ul className="flex flex-col">
+          {latestPosts.map((post) => (
+            <li
+              className="first:border-t first:border-border"
+              key={post.slug}
+            >
+              <PostItem {...post} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 }
