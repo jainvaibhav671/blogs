4fc357e4481233876d945a4dd22d0c82343be81b
Vaibhav Jain <jainvaibhav671@gmail.com>
2024-03-22 13:30:45 +0530

    Fixed typography

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function Page({ params }: PageProps) {
   }
 
   return (
-    <article className="container py-6 dark:prose-invert max-w-3xl mx-auto">
+    <article className="container py-6 prose prose-lg dark:prose-invert max-w-3xl mx-auto">
       <h1 className="mb-2">{post.title}</h1>
       {post.description ? (
         <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
